ccc45cf14d721c5c164a2913761d712468ea8091
Andrius <andr.paulauskas@gmail.com>
2020-01-31 21:57:57 +0200

    initial project commit

---
+++ Source/Necromancy/NecromancyGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "NecromancyGameMode.h"
+#include "NecromancyHUD.h"
+#include "NecromancyCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ANecromancyGameMode::ANecromancyGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = ANecromancyHUD::StaticClass();
+}
---
+++ Source/Necromancy/NecromancyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NecromancyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANecromancyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANecromancyGameMode();
+};
+
+
+
